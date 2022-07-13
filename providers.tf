d0dc97784ae9666966b79523c0c3ec6452c9fb8e
rajeshawsclouddevops <108626091+rajeshawsclouddevops@users.noreply.github.com>
2022-07-13 23:09:55 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,6 +8,6 @@ terraform {
 
 provider "aws" {
   region                  = "us-west-1"
-  shared_credentials_file = "C:/Users/rajes/.aws/credentials"
+  shared_credentials_file = "credentials"
   profile                 = "vscode"
 }
